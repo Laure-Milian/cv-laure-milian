8d14a11d017185c96af1976c985f295ac40e887b
Laure <laure.milian@gmail.com>
2016-12-16 17:02:09 +0100

    gestion menu + mise en forme partie xp et formation

---
+++ app.js
@@ -0,0 +1,35 @@
+
+(function() {
+	$('.div_competences').show();
+	$('.div_formation, .div_XPpro, .div_projets').hide();
+
+	$(".competences").on('click', function() {
+		$('.competences').addClass('active');
+		$('.formation, .XPpro, .projets').removeClass('active');
+		$('.div_competences').show();
+		$('.div_formation, .div_XPpro, .div_projets').hide();
+	})
+
+	$('.XPpro').on('click', function() {
+		$('.XPpro').addClass('active');
+		$('.competences, .formation, .projets').removeClass('active');
+		$('.div_XPpro').show();
+		$('.div_competences, .div_formation, .div_projets').hide();
+	})
+
+	$('.formation').on('click', function() {
+		$('.formation').addClass('active');
+		$('.competences, .XPpro, .projets').removeClass('active');
+		$('.div_formation').show();
+		$('.div_competences, .div_XPpro, .div_projets').hide();
+	})
+
+	$('.projets').on('click', function() {
+		$('.projets').addClass('active');
+		$('.competences, .formation, .XPpro').removeClass('active');
+		$('.div_projets').show();
+		$('.div_competences, .div_formation, .div_XPpro').hide();
+	})
+
+
+})();
