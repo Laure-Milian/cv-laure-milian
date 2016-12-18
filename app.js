ad598e459ecf1ecf528051101d80af23dcf74667
Laure <laure.milian@gmail.com>
2016-12-18 21:36:36 +0100

    add partie competences

--- app.js
+++ app.js
@@ -7,25 +7,8 @@
 
 		init: function() {
 			app.getMenuTopValue();
-			app.listenersMenu();
-		},
-
-		listenersMenu: function() {
-			$('.li_menu').on('click', app.activeMenuItem).bind(this);
-			$('.a_menu').on('click', app.scrollMenu).bind(this);
-
-			$('.a_menu').on('click', app.test).bind(this);
-		},
-
-		activeMenuItem: function() {
-			$('.li_menu').removeClass("active")
-			$(this).addClass("active");
-		},
-
-		scrollMenu: function() {
-			var page = $(this).attr('href');
-			var speed = 700;
-			$('html, body').animate( {scrollTop: $(page).offset().top}, speed);
+			app.listenersMenuClick();
+			app.listenersSectionsScroll();
 		},
 
 		getMenuTopValue: function() {
@@ -42,6 +25,50 @@
 			} else {
 				$('.container_menu').removeClass('sticky');
 			}
+		},
+
+		listenersMenuClick: function() {
+			$('.li_menu').on('click', app.activeMenuItem).bind(this);
+			$('.a_menu').on('click', app.scrollMenu).bind(this);
+		},
+
+		activeMenuItem: function() {
+			$('.li_menu').removeClass("active")
+			$(this).addClass("active");
+		},
+
+		scrollMenu: function() {
+			var page = $(this).attr('href');
+			var speed = 600;
+			$('html, body').animate( {scrollTop: $(page).offset().top - 100}, speed);
+		},
+
+		listenersSectionsScroll: function() {
+			var competencesTopValue = $('#competences').offset().top;
+			var experienceTopValue = $('#experiences_pro').offset().top;
+			var formationTopValue = $('#formation').offset().top;
+			var projetsTopValue = $('#projets').offset().top;
+
+			$(window).scroll(function() {
+				var scrollTop = $(window).scrollTop() + 200;
+				console.log(scrollTop);
+				if (scrollTop > projetsTopValue) {
+					$('.li_menu').removeClass("active");
+					$('.projets').addClass("active");					
+				} 
+				else if (scrollTop > formationTopValue) {
+					$('.li_menu').removeClass("active");
+					$('.formation').addClass("active");
+				} 
+				else if (scrollTop > experienceTopValue) {
+					$('.li_menu').removeClass("active");
+					$('.XPpro').addClass("active");
+				}
+				else {
+					$('.li_menu').removeClass("active");
+					$('.competences').addClass("active");
+				}
+			});
 		}
 
 	}
